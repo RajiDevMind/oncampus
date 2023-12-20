3c11a8bfbbb9198eaff3a469959465c0f859d052
raji <omoiyaalasso@gmail.com>
2023-12-20 03:47:58 +0100

    displayed public file to localhost

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user");
 const connectDB = require("./db/connect");
 const errorHandler = require("./errors/error");
+const { createCustomError } = require("./errors/custom-error");
 const app = express();
+
+// middlewares
+app.use(express.static("./public"));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 4000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(errorHandler);
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static("notification.html"));
+//routes
 app.use("/oncampus", userRoutes);
 
 const start = () => {
@@ -20,7 +24,17 @@ const start = () => {
       await connectDB(process.env.MONGO_URI);
       console.log(`Server is running at http://localhost:${port}`);
     } catch (error) {
-      console.log(error);
+      if (
+        error.hostname === "_mongodb._tcp.oncampuscluster.05kfe2z.mongodb.net"
+      ) {
+        console.log("Unable to connect! check ur internet connection");
+        return createCustomError(
+          "Unable to connect! check ur internet connection"
+        );
+      } else {
+        console.log("Unexpected error", error);
+        return createCustomError("Unexpected error", error);
+      }
     }
   });
 };
