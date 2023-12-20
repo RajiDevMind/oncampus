f640249d45a4464a91bd816ae52fede6006b6d39
raji <omoiyaalasso@gmail.com>
2023-12-20 13:23:24 +0100

    design post form in users profile

--- public/src/acctPage.js
+++ public/src/acctPage.js
@@ -11,9 +11,31 @@ const displayForm = document.querySelector(".displayForm");
 postBtn.addEventListener("click", () => {
   displayForm.classList.toggle("show");
 });
+const remove = document.querySelector(".fa-times");
+remove.addEventListener("click", () => {
+  displayForm.classList.toggle("show");
+});
 
 // change love color while user click
 const love = document.querySelector(".fa-heart");
 love.addEventListener("click", () => {
   love.classList.toggle("color");
+});
+
+const sbtBtn = document.querySelector(".sbtBtn");
+let load = document.querySelector(".loading");
+
+sbtBtn.addEventListener("click", () => {
+  load.style.display = "block";
+  // Simulate a task that takes 2 seconds to complete
+  setTimeout(function () {
+    load.style.display = "none";
+  }, 2000);
+});
+
+// send post to server
+const form = document.querySelector(".form");
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
 });
