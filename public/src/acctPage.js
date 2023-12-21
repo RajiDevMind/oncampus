b99d1379f9982378e6adb22d6ff08d1016a54b32
raji <omoiyaalasso@gmail.com>
2023-12-21 13:15:48 +0100

    postform was stylewithsome functionalities

--- public/src/acctPage.js
+++ public/src/acctPage.js
@@ -36,6 +36,72 @@ sbtBtn.addEventListener("click", () => {
 // send post to server
 const form = document.querySelector(".form");
 
-form.addEventListener("click", (e) => {
+// const preview = document.querySelector(".file");
+
+// preview.addEventListener("onchange", (e) => {
+//   const fileInput = e.target;
+//   const file = fileInput.files[0];
+
+//   if (file) {
+//     const imagePreview = document.querySelector(".image-preview");
+//     imagePreview.innerHTML = `<p>Selected File: ${file.src}</p>`;
+//   }
+// });
+
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
+
+  const files = [];
+  const formData = new FormData(e.currentTarget);
+
+  const getFiles = document.querySelectorAll(".input[type='file']");
+  getFiles.forEach((inputFile) => {
+    const file = inputFile.files[0];
+    files.push(file);
+    formData.append(inputFile.name, file);
+  });
+
+  const values = [...formData.values()];
+
+  if (values.some((value) => value === "")) {
+    displayAlert("Empty input is unacceptable!", "danger");
+    return;
+  } else {
+    displayAlert("Loading...", "success");
+  }
+  console.log(values);
+
+  try {
+    const response = await fetch("http://localhost:4000/oncampus/post", {
+      method: "POST",
+      body: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    if (!response.ok) {
+      displayAlert("Something went wrong, Try again!", "danger");
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    window.location.assign("./src/acctPage.html");
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  e.currentTarget.reset();
 });
+
+// display alert functionality
+const alert = document.querySelector(".alert");
+
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 2000);
+}
